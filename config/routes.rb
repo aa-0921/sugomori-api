7058ea56c1eba9c20f1ef1723619ab73fdc4301b
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-03 13:58:04 +0900

    [add]app/controllers/sessions_controller.rb
    ログイン機能の準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'sessions/new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users
