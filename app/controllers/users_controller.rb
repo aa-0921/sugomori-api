0fe94cd96ad522a9ea2d7aa0e034cc60c225bbdf
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-03 22:03:13 +0900

    [update]app/helpers/sessions_helper.rbにcurrent_user?メソッド追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,7 +61,12 @@ class UsersController < ApplicationController
   def logged_in_user
     return if logged_in?
 
-    flash[:danger] = 'Please log in.'
+    # flash[:danger] = 'Please log in.'
     redirect_to login_url
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
