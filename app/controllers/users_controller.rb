356d2a5c2a33465c7fcf2b6eb407a5c67d96693c
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-03 22:37:50 +0900

    [fix]users_controllerのuser_params修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,8 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:title)
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 
   def logged_in_user
