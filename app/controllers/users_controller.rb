65601a7b8fb596067b3440f95dff7ebc7e8e5f42
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-03 22:40:34 +0900

    [add]users_controllerにadmin_userメソッド追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
   before_action :logged_in_user, only: %i[index edit update]
+  before_action :admin_user,     only: :destroy
 
   def index
     users = User.order(created_at: :desc)
@@ -32,6 +33,8 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     render json: { status: 'SUCCESS', message: 'Deleted the user', data: @user }
+    # flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   def edit
@@ -70,5 +73,9 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
