c2df00e0d23c340bd13183932e67f38a86d6efe9
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-12 17:33:20 +0900

    [fix]app/controllers/sessions_controller.rbのparams.permit等を変更しデバッグを行う

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,40 @@
 class SessionsController < DeviseTokenAuth::SessionsController
   # class SessionsController < Devise::SessionsController
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :sign_in_params
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:name][:session])
+    # p 'よばれていますconfigure_permitted_parameters'
+    # # p email = sign_in_params['email']
+    # email = sign_in_params['email']
+    # p 'email'
+    # p email
+    # password = sign_in_params['password']
+    # p 'password'
+    # p password
+    # p user = User.where(email: email).first
+    # p user.valid_password?(password)
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[session email password])
   end
 
   private
 
   def sign_in_params
-    p params
-    params.permit(:session)
+    # p params
+
+    # p password = sign_in_params['password']
+    # email = sign_in_params['email']
+    # p 'email'
+    # p email
+    # p user = User.where(email: email).first
+    # p user.valid_password?(password)
+    p '呼ばれるsign_in_params。'
+    # p params.require(:session).permit(:email, :password)
+
+    params.permit(:session, :email, :password)
+    # params.require(:session).permit(:email, :password)
     # params.permit(:session: { email: xxx, password: yyyy } )
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,16 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for User.name,
                               at: 'api/v1/auth',
                               controllers: {
-                                registrations: 'api/v1/auth/registrations'
+                                registrations: 'api/v1/auth/registrations',
                                 # omniauth_callbacks: 'omniauth_callbacks',
-                                # sessions: 'sessions'
+                                sessions: 'sessions'
                               }
 
   devise_for :User,
              #  at: 'auth',
              controllers: {
-               omniauth_callbacks: 'omniauth_callbacks',
-               sessions: 'sessions'
+               omniauth_callbacks: 'omniauth_callbacks'
+               #  sessions: 'sessions'
              }
 
   # devise_for :user, controllers: {
