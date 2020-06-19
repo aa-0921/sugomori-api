924c46b3308acd299374631e7c75a0456476ef4c
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-19 23:31:46 +0900

    [fix]picposts_controller.rbのput(:body => params[:picture])を修正し、POST成功

--- app/controllers/picposts_controller.rb
+++ app/controllers/picposts_controller.rb
@@ -3,25 +3,25 @@
 class PicpostsController < ApplicationController
   before_action :set_picpost, only: %i[show update destroy]
 
-  def create
-    picpost = Picpost.create!(picpost_params)
+    def create
+      picpost = Picpost.create!(picpost_params)
 
-    p "picpostの内容"
-    p picpost
-    if picpost.save
-      bucket = Aws::S3::Resource.new(
-        :region => 'ap-northeast-1',
-        :access_key_id => ENV['AWS_ACCCES_KEY'],
-        :secret_access_key => ENV['AWS_ACCCES_SECRET_KEY'],
-      ).bucket('sugomori-app') 
+      p "picpostの内容"
+      p picpost
+      if picpost.save
+        bucket = Aws::S3::Resource.new(
+          :region => 'ap-northeast-1',
+          :access_key_id => ENV['AWS_ACCCES_KEY'],
+          :secret_access_key => ENV['AWS_ACCCES_SECRET_KEY'],
+        ).bucket('sugomori-app') 
 
-    bucket.object("picpost_id_#{picpost.id}_post_image").put(:body => params[:picpost_image])
-    # picpost_image？？
-      render json: { status: 'SUCCESS', data: picpost }
-    else
-      render json: { status: 'ERROR', data: picpost.errors }
+      bucket.object("picpost_id_#{picpost.id}_post_image").put(:body => params[:picture])
+      # picpost_image？？
+        render json: { status: 'SUCCESS', data: picpost }
+      else
+        render json: { status: 'ERROR', data: picpost.errors }
+      end
     end
-  end
 
 
   
