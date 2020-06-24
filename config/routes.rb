0d55403fbd30849e3b2c6eec1792b8823a6dc0e9
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-24 20:58:56 +0900

    likesのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 require 'devise_token_auth'
 Rails.application.routes.draw do
+  get 'likes/create'
+  get 'likes/destroy'
   devise_for :User
   #            controllers: {
   #              omniauth_callbacks: 'omniauth_callbacks'
@@ -9,19 +11,21 @@ Rails.application.routes.draw do
 
   mount_devise_token_auth_for 'User',
                               at: 'api/v1/auth'
-                              # controllers: {
-                              #   registrations: 'api/v1/auth/registrations',
-                              #   sessions: 'api/v1/auth/sessions'
-                              #   omniauth_callbacks: 'omniauth_callbacks'
-                              # }
+  # controllers: {
+  #   registrations: 'api/v1/auth/registrations',
+  #   sessions: 'api/v1/auth/sessions'
+  #   omniauth_callbacks: 'omniauth_callbacks'
+  # }
 
   resources :picposts
   resources :users
   put 'users/follow/:user_id' => 'users#follow'
   put 'users/unfollow/:user_id' => 'users#unfollow'
-  #フォローとフォローを外すアクション
+  # フォローとフォローを外すアクション
 
   get 'users/follow_list/:user_id' => 'users#follow_list'
   get 'users/follower_list/:user_id' => 'users#follower_list'
-  #フォロー・フォロワーの一覧ページ
+  # フォロー・フォロワーの一覧ページ
+
+  resources :likes, only: [:create, :destroy]
 end
