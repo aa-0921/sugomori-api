729566dbe435862cfe60856f37fc8336ceea6565
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-27 19:21:02 +0900

    [fix]controller周辺を修正し、エラーがparse error, malformed request ():のみに

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
+class SessionsController < DeviseTokenAuth::SessionsController
   # before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :authenticate_user!, except: [:new, :create]
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,16 @@ Rails.application.routes.draw do
   # get 'likes/destroy'
   devise_for :User,
              controllers: {
-               omniauth_callbacks: 'omniauth_callbacks'
+               omniauth_callbacks: 'omniauth_callbacks',
              }
 
   mount_devise_token_auth_for 'User',
-                              at: 'api/v1/auth'
-  controllers: {
-    registrations: 'api/v1/auth/registrations',
-    sessions: 'api/v1/auth/sessions'
-    # omniauth_callbacks: 'omniauth_callbacks'
-  }
+                              at: 'api/v1/auth',
+                              controllers: {
+                                registrations: 'api/v1/auth/registrations',
+                                sessions: 'sessions',
+                                # omniauth_callbacks: 'omniauth_callbacks'
+                              }
 
   resources :picposts
   resources :users
