e0972b3b5d424a1c3557d747ccb34b24dc3679d3
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-30 01:01:32 +0900

    [add]authenticate_user追加

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -3,7 +3,7 @@
 class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
   # class SessionsController < Devise::SessionsController
   # before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :authenticate_user!, except: [:new, :create]
+  before_action :authenticate_user!, except: [:new :create]
 
   # respond_to :json
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
   # before_action :logged_in_user, only: %i[edit update]
   before_action :admin_user,     only: :destroy
 
+  # before_action :authenticate_user!, only: %i(edit update)
+
   include ActionController::RequestForgeryProtection
   after_action :set_csrf_token_header
   protect_from_forgery with: :exception
