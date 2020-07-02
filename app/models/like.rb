13e1428945cb8907c04f09b4ef6d893d510d523c
akuto aoyama <aaaa.akuto@gmail.com>
2020-07-02 11:29:21 +0900

    delete old project

--- app/models/like.rb
+++
@@ -1,6 +0,0 @@
-class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :picpost
-  validates :user_id, presence: true
-  validates :picpost_id, presence: true
-end
--- db/migrate/20200617172208_change_datatype_picture_of_picposts.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypePictureOfPicposts < ActiveRecord::Migration[5.2]
-  def change
-    change_column :picposts, :picture, 'text USING CAST(picture AS text)'
-  end
-end
