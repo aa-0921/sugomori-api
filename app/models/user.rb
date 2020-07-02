6124e9cf403c7e65413c142f8b4c91ccf447d256
akuto aoyama <aaaa.akuto@gmail.com>
2020-07-02 23:49:05 +0900

    [fix]models/user.rbの
    deviseログイン、メール認証成功

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,24 @@ class User < ApplicationRecord
          :confirmable, :lockable, :timeoutable
 
   validates :name, presence: true, length: { maximum: 30 }
+
+  acts_as_followable
+  acts_as_follower
+
+  # before_save -> { skip_confirmation! }
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_many :picposts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  has_many :social_profiles, dependent: :destroy
+  def social_profile(provider)
+    social_profiles.select { |sp| sp.provider == provider.to_s }.first
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,21 @@
-# frozen_string_literal: true
+require_relative 'boot'
+
+require 'rails'
+# Pick the frameworks you want:
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+# require "sprockets/railtie"
+require 'rails/test_unit/railtie'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 
 require_relative 'boot'
 
@@ -17,5 +34,13 @@ module SugomoriApi
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Cookies
+    config.action_controller.default_protect_from_forgery = false
+    # deviseを使うのに必要
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.use ActionDispatch::Flash
   end
 end
