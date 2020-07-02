65072288aa57dbafb82c47c816684008be821333
akuto aoyama <aaaa.akuto@gmail.com>
2020-07-02 15:58:33 +0900

    [delete]userモデルの削除

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@
 Rails.application.routes.draw do
   # get 'likes/create'
   # get 'likes/destroy'
-  devise_for :User,
-             controllers: {
-               #  omniauth_callbacks: 'omniauth_callbacks',
-               sessions: 'api/v1/auth/sessions'
-             }
+  # devise_for :User,
+  #            controllers: {
+  #              #  omniauth_callbacks: 'omniauth_callbacks',
+  #              sessions: 'api/v1/auth/sessions'
+  #            }
 
   # mount_devise_token_auth_for 'User',
   #                             at: 'api/v1/auth',
