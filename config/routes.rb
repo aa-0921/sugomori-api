6c0e8179ae9a615b8d7dde553052e5be69bcad75
akuto aoyama <aaaa.akuto@gmail.com>
2020-07-06 15:21:13 +0900

    [change]routes.rbでdeviseのコントローラーを指定

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
   get 'pages/index'
   get 'pages/show'
 
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+  }
+
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   #            controllers: {
