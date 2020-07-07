1440074b3af38634debaa65f7ec377fa0037ecfc
akuto aoyama <aaaa.akuto@gmail.com>
2020-07-07 12:59:07 +0900

    [fix]headerの変更

--- app/javascript/packs/App.tsx
+++ app/javascript/packs/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 
 import { HomePage } from './components/HomePage';
 import './scss/index.scss';
@@ -14,12 +14,12 @@ import { ZeitProvider, CssBaseline } from '@zeit-ui/react';
 //     </ZeitProvider>
 //   );
 // }
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <ZeitProvider>
       <CssBaseline />
       <HomePage />
     </ZeitProvider>,
-    document.body.appendChild(document.createElement("div")),
-  )
-})
+    document.body.appendChild(document.createElement('div')),
+  );
+});
--- app/javascript/packs/components/HomePage.tsx
+++ app/javascript/packs/components/HomePage.tsx
@@ -1,60 +1,59 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
-import { Home } from '../pages/Home';
-import { Pickup } from '../pages/Pickup';
-import { ZeitSample } from '../pages/zeit-sample';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import { Header } from './header';
-import { About } from '../pages/About';
-import { PostsApp } from '../pages/PostsApp';
-import ProfilePage from '../pages/ProfilePage';
-import { MemberListApp } from '../pages/MemberListApp';
-import { FetchData } from '../scripts/api/FetchData';
+// import { Home } from '../pages/Home';
+// import { Pickup } from '../pages/Pickup';
+// import { ZeitSample } from '../pages/zeit-sample';
+// import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+// import { Header } from './header';
+// import { About } from '../pages/About';
+// import { PostsApp } from '../pages/PostsApp';
+// import ProfilePage from '../pages/ProfilePage';
+// import { MemberListApp } from '../pages/MemberListApp';
+// import { FetchData } from '../scripts/api/FetchData';
 
-export const HomePage = () => {
-//   interface ISquareProps {
-//     id: number,
-//     email: string,
-//     name: string,
-  
-  // const [currentUserData, setCurrentUserData] = useState<{}>({})
-// }  // const [currentUserData, setCurrentUserData] = useState({
-  //   id: 0,
-  //   email: '',
-  //   name: '',
-  // })
+// export const HomePage = () => {
+//   // interface ISquareProps {
+//   //     id: number,
+//   //     email: string,
+//   //     name: string,
 
+//   // const [currentUserData, setCurrentUserData] = useState<{}>({})
+//   // }  // const [currentUserData, setCurrentUserData] = useState({
+//   //   id: 0,
+//   //   email: '',
+//   //   name: '',
+//   // })
 
-  // const getInitialDataUrl: string = 'http://localhost:3000/initial_data/show';
+//   // const getInitialDataUrl: string = 'http://localhost:3000/initial_data/show';
 
-  // useEffect(() => {
-  //   FetchData(getInitialDataUrl).then((res) => {
-  //     setCurrentUserData(res.data);
-  //     console.log('getInitialDataUrl', getInitialDataUrl);
-  //     console.log('res.data', res.data);
-  //     console.log('currentUserData', currentUserData);
-  //     console.log('currentUserData');
-  //   });
-  // }, []);
+//   // useEffect(() => {
+//   //   FetchData(getInitialDataUrl).then((res) => {
+//   //     setCurrentUserData(res.data);
+//   //     console.log('getInitialDataUrl', getInitialDataUrl);
+//   //     console.log('res.data', res.data);
+//   //     console.log('currentUserData', currentUserData);
+//   //     console.log('currentUserData');
+//   //   });
+//   // }, []);
 
-  // console.log('currentUserData', currentUserData);
+//   // console.log('currentUserData', currentUserData);
 
-  return (
-    <BrowserRouter>
-      {/* <Header currentUserData={currentUserData}/> */}
-      <Header />
+//   return (
+//     <BrowserRouter>
+//       {/* <Header currentUserData={currentUserData}/> */}
+//       <Header />
 
-      <Switch>
-        <Route exact path="/" component={PostsApp} />
-        <Route exact path="/pickup" component={Pickup} />
-        <Route exact path="/zeit-sample" component={ZeitSample} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/profilepage/:id" component={ProfilePage} />
-        <Route exact path="/MemberListApp" component={MemberListApp} />
+//       <Switch>
+//         <Route exact path="/" component={PostsApp} />
+//         <Route exact path="/pickup" component={Pickup} />
+//         <Route exact path="/zeit-sample" component={ZeitSample} />
+//         <Route exact path="/about" component={About} />
+//         <Route exact path="/profilepage/:id" component={ProfilePage} />
+//         <Route exact path="/MemberListApp" component={MemberListApp} />
 
-        <Route exact path="/postsApp" component={PostsApp} />
-      </Switch>
-      {/* <Footer /> */}
-    </BrowserRouter>
-  );
-};
+//         <Route exact path="/postsApp" component={PostsApp} />
+//       </Switch>
+//       {/* <Footer /> */}
+//     </BrowserRouter>
+//   );
+// };
