0aabe9186d8119d88c6d095684d859d3d93b2677
akuto aoyama <aaaa.akuto@gmail.com>
2020-07-08 11:58:23 +0900

    [fix]tailwindをrailsでも設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,4 @@ require('channels');
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import '../css/tailwindcss.css';
+import 'stylesheets/application';
