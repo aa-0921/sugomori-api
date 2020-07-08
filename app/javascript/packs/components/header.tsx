077ec0cdb468abce5d0e8419936e790c9a6a4cc1
akuto aoyama <aaaa.akuto@gmail.com>
2020-07-08 11:21:55 +0900

    [fix]layoutファイルを編集し、%>が表示されるのを修正

--- app/javascript/packs/components/header.tsx
+++ app/javascript/packs/components/header.tsx
@@ -39,54 +39,51 @@ export function Header() {
                 </div>
                 <div>
                   <div className="hidden md:block">
-                    <div className="ml-10 flex items-baseline">
-                      {currentUserData != null ? (
-                        <>
-                          <Link
-                            to="/"
-                            className="text-lg text-white ml-4 px-3 py-2 rounded-md sm:sm:test-sm font-medium hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                          >
-                            Home
-                          </Link>
-                          <Link
-                            to="/"
-                            className="text-lg text-white ml-4 px-3 py-2 rounded-md sm:test-sm font-medium hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                          >
-                            Feed
-                          </Link>
-                          {/* test */}
-                          <span className="font-semibold text-xl tracking-tight text-white">
-                            SUGOMORI
-                          </span>
-                          <Link
-                            to="/profilepage"
-                            className="text-lg text-white ml-4 px-3 py-2 rounded-md sm:test-sm font-medium  hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                          >
-                            Profile
-                          </Link>
-                          <Link
-                            to="/MemberListApp"
-                            className="text-lg text-white ml-4 px-3 py-2 rounded-md sm:test-sm font-medium hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                          >
-                            MemberInfo
-                          </Link>
-                        </>
-                      ) : (
-                        <>
-                          <Link
-                            to="/"
-                            className="text-lg text-white ml-4 px-3 py-2 rounded-md sm:test-sm font-medium hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                          >
-                            Home
-                          </Link>
-                        </>
-                      )}
-
-                      <div className="ml-3 relative">
-                        <ButtonDropdown size="small">
-                          {currentUserData != null ? (
-                            <>
-                              <ButtonDropdown.Item main>Setting</ButtonDropdown.Item>
+                    <div className="ml-10 flex items-baseline w-auto">
+                      <div className="flex-auto">
+                        {currentUserData != null ? (
+                          <>
+                            <Link
+                              to="/"
+                              className="text-lg text-white ml-4 px-3 py-2 rounded-md sm:sm:test-sm font-medium hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+                            >
+                              Home
+                            </Link>
+                            <Link
+                              to="/"
+                              className="text-lg text-white ml-4 px-3 py-2 rounded-md sm:test-sm font-medium hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+                            >
+                              Feed
+                            </Link>
+                            <Link
+                              to="/profilepage"
+                              className="text-lg text-white ml-4 px-3 py-2 rounded-md sm:test-sm font-medium  hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+                            >
+                              Profile
+                            </Link>
+                            <Link
+                              to="/MemberListApp"
+                              className="text-lg text-white ml-4 px-3 py-2 rounded-md sm:test-sm font-medium hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+                            >
+                              MemberInfo
+                            </Link>
+                          </>
+                        ) : (
+                          <>
+                            <Link
+                              to="/"
+                              className="text-lg text-white ml-4 px-3 py-2 rounded-md sm:test-sm font-medium hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+                            >
+                              Home
+                            </Link>
+                          </>
+                        )}
+                      </div>
+                      <div className="flex-auto float-right">
+                        {currentUserData != null ? (
+                          <>
+                            <ButtonDropdown size="small">
+                              <ButtonDropdown.Item main>Settings</ButtonDropdown.Item>
                               <ButtonDropdown.Item>
                                 <a
                                   href="/users/edit"
@@ -105,77 +102,23 @@ export function Header() {
                                   ログアウト
                                 </a>
                               </ButtonDropdown.Item>
-                            </>
-                          ) : (
-                            <>
-                              <ButtonDropdown.Item main>Setting</ButtonDropdown.Item>
-                              <ButtonDropdown.Item>
-                                <a href="/pages/index">Login</a>
+                            </ButtonDropdown>
+                          </>
+                        ) : (
+                          <>
+                            <ButtonDropdown size="small">
+                              <ButtonDropdown.Item main>
+                                <a href="/pages/index">ログイン</a>
                               </ButtonDropdown.Item>
                               <ButtonDropdown.Item>
-                                <a href="/users/sign_in">サインイン</a>
+                                <a href="/users/sign_in">新規登録</a>
                               </ButtonDropdown.Item>
-                            </>
-                          )}
-                        </ButtonDropdown>
+                            </ButtonDropdown>
+                          </>
+                        )}
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className="hidden md:block">
-                  <div className="ml-4 flex items-center md:ml-6">
-                    <button
-                      className="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
-                      aria-label="Notifications"
-                    >
-                      <svg
-                        className="h-6 w-6"
-                        stroke="currentColor"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-                <div className="-mr-2 flex md:hidden">
-                  {/* <!-- Mobile menu button --> */}
-                  <button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white">
-                    {/* <!-- Menu open: "hidden", Menu closed: "block" --> */}
-                    <svg
-                      className="block h-6 w-6"
-                      stroke="currentColor"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4 6h16M4 12h16M4 18h16"
-                      />
-                    </svg>
-                    {/* <!-- Menu open: "block", Menu closed: "hidden" --> */}
-                    <svg
-                      className="hidden h-6 w-6"
-                      stroke="currentColor"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </button>
                 </div>
               </div>
             </div>
