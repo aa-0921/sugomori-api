96ae338eb3feab9d243b6cf786d52ccc463a8139
akuto aoyama <aaaa.akuto@gmail.com>
2020-07-08 17:55:24 +0900

    [fix]config/webpack/development.jsの設定修正

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -3,6 +3,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const environment = require('./environment');
 module.exports = merge(environment.toWebpackConfig().delete('entry'), {
   entry: {
-    home: '/app/app/javascript/packs/App.jsx',
+    App: '/app/app/javascript/packs/App.jsx',
   },
 });
