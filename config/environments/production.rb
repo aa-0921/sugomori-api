8240f6848e089d8a61a399af5751aaef2c4a282f
akuto aoyama <aaaa.akuto@gmail.com>
2020-07-14 13:36:20 +0900

    [add]config.action_mailer.default_url_options = { host: 'example.com'}に変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,6 +109,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.routes.default_url_options[:host] = host
+
+  # config.routes.default_url_options[:host] = host
+  config.action_mailer.default_url_options = { host: 'example.com'}
 
 end
