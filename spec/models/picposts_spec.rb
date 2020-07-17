51d06d82736182119e599ec90e598e01a7c267b7
akuto aoyama <aaaa.akuto@gmail.com>
2020-07-17 13:02:57 +0900

    [add]/picposts_spec.rbにいいねのテスト追加

--- spec/models/picposts_spec.rb
+++ spec/models/picposts_spec.rb
@@ -1,9 +1,16 @@
 RSpec.describe Picpost, type: :model, focus: true do
-  let(:picpost) { build(:picpost) }
+  let(:user) { create(:user) }
+  let(:picpost) { create(:picpost) }
 
   it { is_expected.to validate_presence_of(:picture) }
   it { is_expected.to validate_presence_of(:user_id) }
   it { should validate_length_of(:content).is_at_most(30) }
+
   it { is_expected.to have_many(:comments) }
   it { is_expected.to have_many(:likes) }
+
+  it 'userが投稿すると、Likeテーブルにレコードが一つ追加される' do
+    picpost.save
+    expect { picpost.iine(user) }.to change(Like, :count).by(1)
+  end
 end
