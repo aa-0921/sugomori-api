59aae63935544e4131225028c297ded6f50e1b2f
akuto aoyama <aaaa.akuto@gmail.com>
2020-07-18 13:18:18 +0900

    [fix]followのテスト変更し、7つ通過

--- spec/factories/follow.rb
+++ spec/factories/follow.rb
@@ -2,6 +2,14 @@ FactoryBot.define do
   factory :follow do
     sequence(:id) { |n| n }
     sequence(:follower_id) { |n| n }
+    sequence(:followable_id) { |n| n + 1 }
+    followable_type { "User" }
+    follower_type { "User" }
+  end
+
+  factory :unfollow do
+    sequence(:id) { |n| n }
+    sequence(:follower_id) { |n| n }
     sequence(:followable_id) { |n| n }
   end
 end
--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -2,6 +2,8 @@ RSpec.describe Follow, type: :model, focus: true do
   let(:user) { create(:user) }
   let(:another_user) { create(:user) }
   let(:follow) { build(:follow) }
+  # let(:unfollow) { build(:unfollow) }
+
   # let(:follow) { create(:follow, follower_id: user.id, followable_id: another_user.id) }
 
   # before do
@@ -19,20 +21,45 @@ RSpec.describe Follow, type: :model, focus: true do
     # f.save
   end
 
-  # preset等のバリデーション関連
-  it { is_expected.to validate_presence_of(:followable_id) }
-  it { is_expected.to validate_presence_of(:follower_id) }
+  # present等のバリデーション関連
+  it "follower_idが入っていなければ無効である" do
+    follow = Follow.new(
+      follower_id: nil,
+      followable_id: 1
+    )
+    expect(follow).to be_invalid
+  end
+
+  it "followable_idが入っていなければ無効である" do
+    follow = Follow.new(
+      follower_id: 1,
+      followable_id: nil
+    )
+    expect(follow).to be_invalid
+  end
+
+  # it "followable_idが入っていなければ無効である" do
+  #   follow = Follow.new(
+  #     follower_id: 1,
+  #     followable_id: 2
+  #   )
+  #   expect(validate_presence_of(:followable_id)).to eq true
+  # end
+
+  # it { is_expected.to validate_presence_of(:followable_id) }
+  # it { is_expected.to validate_presence_of(:follower_id) }
 
   it { should belong_to(:followable) }
   it { should belong_to(:follower) }
 
-  # いいね関連
   it 'userがフォローすると、Followテーブルにレコードが一つ追加される' do
+    follow.save
+    p follow
     expect { user.follow(another_user) }.to change(Follow, :count).by(1)
   end
 
   it 'userがフォローを解除するとFollowテーブルからレコードが一つ消える' do
     user.follow(another_user)
-    expect { user.unfollow(another_user) }.to change(Follow, :count).by(-1)
+    expect { user.stop_following(another_user) }.to change(Follow, :count).by(-1)
   end
 end
