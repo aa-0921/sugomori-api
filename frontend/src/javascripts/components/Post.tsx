60fd5eb204a3e95d85e5ac2fe403e92fe070bf30
akuto aoyama <aaaa.akuto@gmail.com>
2020-07-26 10:08:25 +0900

    [add]tilt追加

--- frontend/src/javascripts/components/Post.tsx
+++ frontend/src/javascripts/components/Post.tsx
@@ -1,18 +1,34 @@
 import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Tilt from 'react-parallax-tilt';
 
 export const Post = (props: any) => {
   return (
     <React.Fragment>
-      <div className="">
-        <img
-          key={props.post.id}
-          src={props.post.picture}
-          className="rounded-lg cursor-pointer transform hover:scale-105 hover:skew-y-3 hover:translate-y-1 duration-500 ease-out"
-          onClick={() => props.modalOpenHandler(props.post)}
-        />
 
-      </div>
+      <Tilt
+        tiltReverse={true}
+        glareEnable={true}
+        glareMaxOpacity={0.45}
+        glarePosition="all"
+      >
+        {/* <div style={{ height: '300px', backgroundColor: 'darkgreen' }}> */}
+        <div>
+          <img
+            key={props.post.id}
+            src={props.post.picture}
+            className="rounded-lg cursor-pointer"
+            // className="rounded-lg cursor-pointer transform hover:scale-105 hover:skew-y-3 hover:translate-y-1 duration-500 ease-out"
+            onClick={() => props.modalOpenHandler(props.post)}
+          />
+          <div>
+            <p className="opacity-50">Click!!</p>
+          </div>
+        </div>
 
+
+        {/* </div> */}
+      </Tilt>
     </React.Fragment>
   );
 };
